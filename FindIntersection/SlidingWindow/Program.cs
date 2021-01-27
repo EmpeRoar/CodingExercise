e19ec13e753b1a0e58b57f9f92cb42bfc57edcbf
Julius Bacosa <juliusbacosa@gmail.com>
2021-01-27 14:52:05 +0800

    .

--- FindIntersection/SlidingWindow/Program.cs
+++ FindIntersection/SlidingWindow/Program.cs
@@ -87,11 +87,13 @@ Your program should return the Moving Median for each element based on the
 element and its N-1 predecessors, where N is the sliding window size. 
 
 The final output should 
-be a string with the moving median corresponding to each entry in the original array separated by commas.
+be a string with the moving median corresponding to each entry in the original 
+array separated by commas.
 
-Note that for the first few elements (until the window size is reached), the median is computed 
-on a smaller number of entries. 
-For example: if arr is [3, 1, 3, 5, 10, 6, 4, 3, 1] then your program should output "1,2,3,5,6,6,4,3"
+Note that for the first few elements (until the window size is reached), 
+the median is computed on a smaller number of entries. 
+For example: if arr is [3, 1, 3, 5, 10, 6, 4, 3, 1] then your program should 
+output "1,2,3,5,6,6,4,3"
 
 
 Input: new int[] {5, 2, 4, 6}
