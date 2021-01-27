8988edea32496054ee5939168a43372e008715d2
Julius Bacosa <juliusbacosa@gmail.com>
2021-01-27 20:51:33 +0800

    .

---
+++ Play/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Play
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 888;
+            var list = new List<string>(){
+                $"Hello {i}",
+                $"World {i}"    
+            };
+
+            var arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            var listOfNumbers = new int[arr.Length - 4];
+
+            // source arr
+            // source index
+            // dest arr
+            // dest index
+            // size
+
+            Array.Copy(arr, 4, listOfNumbers, 0, arr.Length - 4);
+            
+
+            var strs = "1, 2, 3, 4, 5, 6";
+            var strArr = strs.Split(",");
+
+            foreach(var str in strArr){
+                Console.WriteLine($"--{str.Trim()}--");
+            }
+
+            Console.WriteLine(string.Join(" ", listOfNumbers));
+        }
+    }
+}
