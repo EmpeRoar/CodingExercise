07cd3d62547fdf3b1df321ce931c6219730af214
Julius Bacosa <juliusbacosa@gmail.com>
2021-02-24 16:33:00 +0800

    test

---
+++ Play/Play/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Play
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public DateTime DT { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = new List<Dog>()
+            {
+                new Dog(){ Name = "JL", DT = DateTime.Now },
+                new Dog(){ Name = "EG", DT = DateTime.Now }
+            };
+
+            
+
+            var names = str.Select(x => x.Name).ToList();
+            var many = str.SelectMany(x => x, (o, z) => new { o.Name, z  }).ToList();
+
+            Console.WriteLine(String.Join(",", names.ToArray()));
+            Console.WriteLine(String.Join(",", many.ToArray()));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Playcode/Anagram/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Anagram
+{
+    /// <summary>
+    /// This is the Program!!!
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// This function is to determine if the 2 strings are Anagram
+        /// </summary>
+        /// <param name="str1"></param>
+        /// <param name="str2"></param>
+        /// <returns></returns>
+        static bool IsAnagram(string str1, string str2)
+        {
+            var str1R = str1.ToLower().ToArray();
+            var str2R = str2.ToLower().ToArray();
+            Array.Sort(str1R);
+            Array.Sort(str2R);
+
+            return new string(str1R) == new string(str2R);
+        }
+
+        /// <summary>
+        /// This is the Main Function!!!
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsAnagram("Jupiter", "rupjite"));
+            Console.ReadLine();
+        }
+    }
+}
