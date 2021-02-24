74fa50876cdd561c8e82e005428d7c5e675ec230
Julius Bacosa <juliusbacosa@gmail.com>
2021-02-24 18:01:12 +0800

    test

--- Visio/Autumn/Autumn.ConsoleApp/Program.cs
+++ Visio/Autumn/Autumn.ConsoleApp/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Autumn.ConsoleApp
 {
+    /// <summary>
+    /// 
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int m = 5;
+            int[] s = new int[] { 1, 2,};
+
+            
         }
     }
 }
