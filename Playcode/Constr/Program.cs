86465ff43ce6bb192f9edb054b3b3c69116af0d8
Julius Bacosa <juliusbacosa@gmail.com>
2021-02-25 16:56:34 +0800

    fibo

--- Playcode/Constr/Program.cs
+++ Playcode/Constr/Program.cs
@@ -4,28 +4,38 @@ namespace Constr
 {
     public class BaseClass
     {
+        protected string _baseVal;
         public BaseClass(string val)
         {
+            _baseVal = val;
             Console.WriteLine($"I am Base!! {val}");
         }
 
-        public int Add(int var1, int var2)
-        {
-            return var1 + var2;
-        }
+        //public int Add(int var1, int var2)
+        //{
+        //    return var1 + var2;
+        //}
     }
 
     public class ChildClass : BaseClass
     {
+        private string _val;
         public ChildClass(string val) : base(val)
         {
+            _val = val;
             Console.WriteLine($"I am Child!!! {val}");
         }
 
-        public int Multiply(int var1, int var2)
+        public string Say()
         {
-            return var1 * var2;
+            var v = _baseVal;
+            return $"{_val} {v}";
         }
+
+        //public int Multiply(int var1, int var2)
+        //{
+        //    return var1 * var2;
+        //}
     }
 
     class Program
@@ -35,8 +45,11 @@ namespace Constr
             //BaseClass b = new ChildClass("Hello World!!!");
             //b.Add(1, 1);
 
-            ChildClass c = (ChildClass) new BaseClass("Hello Child Class");
-            c.Multiply(2, 2);
+            //ChildClass c = (ChildClass) new BaseClass("Hello Child Class");
+            //c.Multiply(2, 2);
+
+            var c = new ChildClass("Hello");
+            Console.WriteLine(c.Say());
             Console.ReadLine();
         }
     }
--- Playcode/Fibonacci/Program.cs
+++ Playcode/Fibonacci/Program.cs
@@ -4,27 +4,9 @@ namespace Fibonacci
 {
     class Program
     {
-        // 0, 1, 1, 2, 3, 5, 8, 13, 21
         static void Main(string[] args)
         {
-            int a = 0;
-            int b = 1;
-
-            Console.WriteLine($"{a}");
-            
-            for (int i = 0; i <= 10; i++)
-            {
-                int temp = a;
-                a = b;
-                b = temp + a; // next iteration...
-
-                // temp = 1;
-                // a = 2;
-                // b = 1 + 2 = 3;
-                Console.WriteLine($"{a}");
-                // 1,1,2
-            }
-
+           
 
             Console.ReadLine();
         }
