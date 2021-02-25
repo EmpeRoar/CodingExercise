ddb12fbd9727ee42bd0a8be9ad13029e3ce3266e
Julius Bacosa <juliusbacosa@gmail.com>
2021-02-25 17:31:18 +0800

    fib

--- Playcode/Fibonacci/Program.cs
+++ Playcode/Fibonacci/Program.cs
@@ -2,12 +2,31 @@
 
 namespace Fibonacci
 {
+    /// <summary>
+    /// Fibonacci Sequence for Noobs!!!!
+    /// </summary>
     class Program
     {
+        // 1, 1, 2, 3, 5, 8, 13, 21, 34
+        // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
         static void Main(string[] args)
         {
-           
-
+            int next = 0;
+            int prev = 0;
+            for(int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(next);
+                if (next == 0)
+                {
+                    next = 1;
+                }
+                else
+                {
+                    int temp = next;
+                    next = next + prev;
+                    prev = temp;
+                }
+            }
             Console.ReadLine();
         }
     }
