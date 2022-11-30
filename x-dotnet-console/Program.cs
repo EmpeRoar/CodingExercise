ac2047d2cfbfae5e9ae9efbf88cac25abc6fa2bb
Julius Bacosa <juliusbacosa@gmail.com>
2022-11-30 13:32:01 +0000

    ...

---
+++ x-dotnet-console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace x_dotnet_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            String str = "Julz";
+            System.Console.WriteLine($"{str}");
+        }
+    }
+}
